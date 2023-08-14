2fed3400880375ee108dc2c784593ad0ac03b4dc
Dhanraj <dhanrajinstru099@gmail.com>
2023-08-14 22:04:24 +0530

    first-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/components/home/home.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { LoginComponent } from './shared/components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LoginComponent},
+  {path: "home", component: HomeComponent},
+  {path: "pageNotFound", component: PageNotFoundComponent},
+  {path: "**", redirectTo:"pageNotFound" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
